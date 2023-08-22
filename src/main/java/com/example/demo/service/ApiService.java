ee9b02d5b46d4ed390b3cc0d2fd5c5e087507155
LAKSAN7 <Laksanandpowerl@gmail.com>
2023-08-22 15:06:55 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/ApiService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import java.util.List; 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepo;
+
+@Service
+public class ApiService {
+	@Autowired
+	EmployeeRepo er;
+	
+	public Employee saveinfo(Employee e)
+	{
+		return er.save(e);
+	}
+	
+	public List<Employee> showinfo()
+	{
+		return er.findAll();
+	}
+
+	public Employee getEmployeeById(Long id) {
+		Optional<Employee> employeeOptional = er.findById(id);
+        return employeeOptional.orElse(null);
+	}
+}
